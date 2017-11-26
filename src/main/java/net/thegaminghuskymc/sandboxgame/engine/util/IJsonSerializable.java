d4c7a18c6f8f69252370f0115eee268b3fcb9c79
TheGamingHuskyMC <sindrefagerheim53@gmail.com>
2017-11-26 10:55:48 +0100

    Gonna be used to resourcepacks(wip)

---
+++ src/main/java/net/thegaminghuskymc/sandboxgame/engine/util/IJsonSerializable.java
@@ -0,0 +1,13 @@
+package net.thegaminghuskymc.sandboxgame.engine.util;
+
+import com.google.gson.JsonElement;
+
+public interface IJsonSerializable
+{
+    void fromJson(JsonElement json);
+
+    /**
+     * Gets the JsonElement that can be serialized.
+     */
+    JsonElement getSerializableElement();
+}
