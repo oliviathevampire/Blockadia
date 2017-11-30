e630e2767e0939d187ad64cc622b76078bc493e9
lukas2005 <lukas2005.38@gmail.com>
2017-11-30 18:31:28 +0100

    fixed

--- src/main/java/net/thegaminghuskymc/sandboxgame/engine/world/World.java
+++ src/main/java/net/thegaminghuskymc/sandboxgame/engine/world/World.java
@@ -1,8 +1,8 @@
 package net.thegaminghuskymc.sandboxgame.engine.world;
 
 import net.thegaminghuskymc.sandboxgame.engine.Scene;
-import net.thegaminghuskymc.sandboxgame.engine.Entity;
 import net.thegaminghuskymc.sandboxgame.engine.block.Block;
+import net.thegaminghuskymc.sandboxgame.engine.entities.Entity;
 import net.thegaminghuskymc.sandboxgame.engine.graph.Material;
 import net.thegaminghuskymc.sandboxgame.engine.graph.Mesh;
 import org.joml.Vector3f;
@@ -14,7 +14,7 @@ import java.util.HashMap;
 public class World {
 
     public final WorldType type = WorldType.DEFAULT;
-    public ArrayList<Entity> entities = new ArrayList<Entity>();
+    public ArrayList<Entity> entities = new ArrayList<>();
     public final HashMap<Vector3f, Block> blocks = new HashMap<>();
     public final ArrayList<Block> notAddedToScene = new ArrayList<>();
 
