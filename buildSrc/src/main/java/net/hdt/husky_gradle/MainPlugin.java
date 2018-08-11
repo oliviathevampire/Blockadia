80c7f7453ce7cde2be27e8b2a5c2baeb2fd3da01
TheGamingHuskyMC <sindrefagerheim53@gmail.com>
2018-08-11 19:35:41 +0200

    Working on the plugin

--- buildSrc/src/main/java/net/hdt/husky_gradle/MainPlugin.java
+++ buildSrc/src/main/java/net/hdt/husky_gradle/MainPlugin.java
@@ -7,7 +7,10 @@ public class MainPlugin implements Plugin<Project> {
 
     @Override
     public void apply(Project project) {
-
+        project.getTasks().create("hello", Greeting.class, (task) -> {
+            task.setMessage("Hello");
+            task.setRecipient("World");
+        }).setDescription("This is just a test command");
     }
 
 }
