0de46bdff8ab7f66b1c3e4c6a5fb9824a5031dee
Tebreca <ewan.arends10@gmail.com>
2018-08-12 20:58:58 +0200

    add todo for myself

--- src/main/java/team/hdt/sandboxgame/game_engine/common/world/gen/factory/WorldFactory.java
+++ src/main/java/team/hdt/sandboxgame/game_engine/common/world/gen/factory/WorldFactory.java
@@ -2,5 +2,6 @@ package team.hdt.sandboxgame.game_engine.common.world.gen.factory;
 
 public class WorldFactory {
 
+//TODO: gen x lines, make it select 2 biometypes, from x-1 chunk and y-1 chunk, and grab random one
 
 }
