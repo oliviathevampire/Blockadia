56d7a40b0a1a1bac4539f2e576cf411f0be5b438
gegy1000 <gegy1000@gmail.com>
2018-08-12 17:38:31 +0200

    GameController and tick regulation

--- src/main/java/team/hdt/sandboxgame/game_engine/common/Main.java
+++ src/main/java/team/hdt/sandboxgame/game_engine/common/Main.java
@@ -1,9 +1,13 @@
 package team.hdt.sandboxgame.game_engine.common;
 
 import team.hdt.sandboxgame.game_engine.common.util.Display;
+import team.hdt.sandboxgame.game_engine.common.util.TickRegulator;
 import team.hdt.sandboxgame.game_engine.common.world.block.BlockTypes;
 
 public class Main {
+
+    private static final int FRAME_RATE = 60;
+    private static final int TICK_RATE = 20;
 
     public static int WINDOW_WIDTH = 856, WINDOW_HEIGHT = 480;
     public static Display display;
@@ -12,7 +16,32 @@ public class Main {
         // TODO: Some centralised place for registration of everything
         BlockTypes.register();
 
+        GameController controller = new GameController();
+        controller.setup();
+
         display = new Display("Husky's Sandbox Game", WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        TickRegulator tickRegulator = new TickRegulator(1000000000L / TICK_RATE);
+        TickRegulator frameRegulator = new TickRegulator(1000000000L / FRAME_RATE);
+
+        tickRegulator.start();
+        frameRegulator.start();
+
+        while (!display.shouldClose()) {
+            // TODO: This setup is not ideal.
+            int scheduledFrames = frameRegulator.getScheduledTicks();
+            for (int i = 0; i < scheduledFrames; i++) {
+                controller.render();
+            }
+
+            int scheduledTicks = tickRegulator.getScheduledTicks();
+            for (int i = 0; i < scheduledTicks; i++) {
+                controller.update();
+            }
+        }
+
         display.run();
+
+        controller.drop();
     }
 }
--- src/main/java/team/hdt/sandboxgame/game_engine/common/state/GameState.java
+++ src/main/java/team/hdt/sandboxgame/game_engine/common/state/GameState.java
@@ -3,5 +3,9 @@ package team.hdt.sandboxgame.game_engine.common.state;
 public interface GameState {
     void setup();
 
+    void drop();
+
+    GameState update();
+
     void render();
 }
