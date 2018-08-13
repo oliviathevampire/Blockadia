71768759a5051047279949be78818da53b765e4e
TheGamingHuskyMC <sindrefagerheim53@gmail.com>
2018-08-13 19:43:32 +0200

    Fixed some stuff

--- src/main/java/team/hdt/blockadia/test/DummyGame.java
+++ src/main/java/team/hdt/blockadia/test/DummyGame.java
@@ -4,18 +4,14 @@ import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 import org.joml.Vector2f;
 import org.joml.Vector3f;
-import team.hdt.blockadia.game_engine.common.registry.BiomeRegistry;
-import team.hdt.blockadia.game_engine.common.world.ChunkPos;
 import team.hdt.blockadia.game_engine.common.world.World;
-import team.hdt.blockadia.game_engine.common.world.biomes.BiomeFlatlands;
 import team.hdt.blockadia.game_engine.common.world.biomes.Biomes;
 import team.hdt.blockadia.game_engine.common.world.block.BlockTypes;
+import team.hdt.blockadia.game_engine.common.world.block.BlockUtils;
 import team.hdt.blockadia.game_engine.common.world.gen.factory.WorldFactory;
-import team.hdt.blockadia.game_engine.common.world.gen.interfaces.IBiome;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.io.OutputStream;
 
 import static org.lwjgl.glfw.GLFW.*;
 
@@ -50,105 +46,28 @@ public class DummyGame implements IGameLogic {
         File f = new File("world.xml");
         f.createNewFile();
         outputter.output(world.toXML(), new FileWriter(f));
+
         renderer.init(window);
-        // Create the Mesh
-        float[] positions = new float[]{
-            // V0
-            -0.5f, 0.5f, 0.5f,
-            // V1
-            -0.5f, -0.5f, 0.5f,
-            // V2
-            0.5f, -0.5f, 0.5f,
-            // V3
-            0.5f, 0.5f, 0.5f,
-            // V4
-            -0.5f, 0.5f, -0.5f,
-            // V5
-            0.5f, 0.5f, -0.5f,
-            // V6
-            -0.5f, -0.5f, -0.5f,
-            // V7
-            0.5f, -0.5f, -0.5f,
-            // For text coords in top face
-            // V8: V4 repeated
-            -0.5f, 0.5f, -0.5f,
-            // V9: V5 repeated
-            0.5f, 0.5f, -0.5f,
-            // V10: V0 repeated
-            -0.5f, 0.5f, 0.5f,
-            // V11: V3 repeated
-            0.5f, 0.5f, 0.5f,
-            // For text coords in right face
-            // V12: V3 repeated
-            0.5f, 0.5f, 0.5f,
-            // V13: V2 repeated
-            0.5f, -0.5f, 0.5f,
-            // For text coords in left face
-            // V14: V0 repeated
-            -0.5f, 0.5f, 0.5f,
-            // V15: V1 repeated
-            -0.5f, -0.5f, 0.5f,
-            // For text coords in bottom face
-            // V16: V6 repeated
-            -0.5f, -0.5f, -0.5f,
-            // V17: V7 repeated
-            0.5f, -0.5f, -0.5f,
-            // V18: V1 repeated
-            -0.5f, -0.5f, 0.5f,
-            // V19: V2 repeated
-            0.5f, -0.5f, 0.5f,};
-        float[] textCoords = new float[]{
-            0.0f, 0.0f,
-            0.0f, 0.5f,
-            0.5f, 0.5f,
-            0.5f, 0.0f,
-            0.0f, 0.0f,
-            0.5f, 0.0f,
-            0.0f, 0.5f,
-            0.5f, 0.5f,
-            // For text coords in top face
-            0.0f, 0.5f,
-            0.5f, 0.5f,
-            0.0f, 1.0f,
-            0.5f, 1.0f,
-            // For text coords in right face
-            0.0f, 0.0f,
-            0.0f, 0.5f,
-            // For text coords in left face
-            0.5f, 0.0f,
-            0.5f, 0.5f,
-            // For text coords in bottom face
-            0.5f, 0.0f,
-            1.0f, 0.0f,
-            0.5f, 0.5f,
-            1.0f, 0.5f,};
-        int[] indices = new int[]{
-            // Front face
-            0, 1, 3, 3, 1, 2,
-            // Top Face
-            8, 10, 11, 9, 8, 11,
-            // Right face
-            12, 13, 7, 5, 12, 7,
-            // Left face
-            14, 15, 6, 4, 14, 6,
-            // Bottom face
-            16, 18, 19, 17, 16, 19,
-            // Back face
-            4, 6, 7, 5, 4, 7,};
-        Texture texture = new Texture("/textures/grassblock.png");
-        Mesh mesh = new Mesh(positions, textCoords, indices, texture);
+
+        Texture texture = new Texture("/textures/grass.png");
+        Mesh mesh = new Mesh(BlockUtils.positions, BlockUtils.textCoords, BlockUtils.indices, texture);
+
         GameItem gameItem1 = new GameItem(mesh);
         gameItem1.setScale(0.5f);
         gameItem1.setPosition(0, 0, -2);
+
         GameItem gameItem2 = new GameItem(mesh);
         gameItem2.setScale(0.5f);
         gameItem2.setPosition(0.5f, 0.5f, -2);
+
         GameItem gameItem3 = new GameItem(mesh);
         gameItem3.setScale(0.5f);
         gameItem3.setPosition(0, 0, -2.5f);
+
         GameItem gameItem4 = new GameItem(mesh);
         gameItem4.setScale(0.5f);
         gameItem4.setPosition(0.5f, 0, -2.5f);
+
         gameItems = new GameItem[]{gameItem1, gameItem2, gameItem3, gameItem4};
     }
 
@@ -165,9 +84,9 @@ public class DummyGame implements IGameLogic {
         } else if (window.isKeyPressed(GLFW_KEY_D)) {
             cameraInc.x = 1;
         }
-        if (window.isKeyPressed(GLFW_KEY_Z)) {
+        if (window.isKeyPressed(GLFW_KEY_LEFT_SHIFT) || window.isKeyPressed(GLFW_KEY_RIGHT_SHIFT)) {
             cameraInc.y = -1;
-        } else if (window.isKeyPressed(GLFW_KEY_X)) {
+        } else if (window.isKeyPressed(GLFW_KEY_SPACE)) {
             cameraInc.y = 1;
         }
     }
