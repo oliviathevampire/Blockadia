eed6a41b24d5ef615062ef4f6f0bc92b9deba03b
TheGamingHuskyMC <sindrefagerheim53@gmail.com>
2018-08-21 15:24:58 +0200

    Added just a test mod and fixed some imports

---
+++ src/main/java/team/hdt/test_mod/TestMod.java
@@ -0,0 +1,35 @@
+package team.hdt.test_mod;
+
+import team.hdt.blockadia.mod_engine.interfaces.IMod;
+
+/*
+ * This class is made by HuskyTheArtist
+ * the 21.08.2018 at 09.14
+ */
+public class TestMod implements IMod {
+    @Override
+    public String getDisplayName() {
+        return "Test Mod";
+    }
+
+    @Override
+    public String getModID() {
+        return "test_mod";
+    }
+
+    @Override
+    public String getResourceLocation() {
+        return "assets/testmod";
+    }
+
+    @Override
+    public String getVersion() {
+        return "0.0.1";
+    }
+
+    @Override
+    public String getDescription() {
+        return "This is a test mod";
+    }
+
+}
