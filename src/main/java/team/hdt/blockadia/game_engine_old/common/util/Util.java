6a08fd3d06530787b7360f0e2a2acc2266bb7cf7
Mystic4pheonix <Lukethemodded@gmail.com>
2018-08-26 12:28:53 +0100

    update

--- src/main/java/team/hdt/blockadia/game_engine_old/common/util/Util.java
+++ src/main/java/team/hdt/blockadia/game_engine_old/common/util/Util.java
@@ -9,8 +9,8 @@ import com.google.common.collect.Iterators;
 import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import team.hdt.blockadia.game_engine_old.common.util.interfaces.Nullable;
 
-import javax.annotation.Nullable;
 import java.io.File;
 import java.io.IOException;
 import java.lang.management.ManagementFactory;
