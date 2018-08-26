59a38f565444bccc2e4784d6eab59ca3d478591e
TheGamingHuskyMC <sindrefagerheim53@gmail.com>
2018-08-26 13:31:36 +0200

    Fixed?

--- src/main/java/team/hdt/blockadia/game_engine/client/BlockadiaClient.java
+++ src/main/java/team/hdt/blockadia/game_engine/client/BlockadiaClient.java
@@ -10,13 +10,7 @@ public class BlockadiaClient {
     private static final int CLIENT_WIDTH = 856, CLIENT_HEIGHT = 480;
 
     public static void main(String[] args) {
-        String title;
-        if(args[0] != null) {
-            title = args[0];
-        } else {
-            title = "Blockadia";
-        }
-        Display display = new Display(title, CLIENT_WIDTH, CLIENT_HEIGHT);
+        Display display = new Display("Blockadia", CLIENT_WIDTH, CLIENT_HEIGHT);
         display.run();
     }
 
