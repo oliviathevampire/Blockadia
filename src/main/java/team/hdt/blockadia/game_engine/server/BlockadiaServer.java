a23a334deee364c5e4cd45bd8b9441b9f6eced46
Mystic4pheonix <Lukethemodded@gmail.com>
2018-09-04 18:39:56 +0100

    k

--- src/main/java/team/hdt/blockadia/game_engine/server/BlockadiaServer.java
+++ src/main/java/team/hdt/blockadia/game_engine/server/BlockadiaServer.java
@@ -1,16 +1,15 @@
 package team.hdt.blockadia.game_engine.server;
 
+import team.hdt.blockadia.game_engine.client.MainExtras;
 import team.hdt.blockadia.game_engine.core.Display;
 import team.hdt.blockadia.game_engine.core.util.GameSide;
 import team.hdt.blockadia.game_engine.core.util.GameSideOnly;
 
 @GameSideOnly(GameSide.SERVER)
-public class BlockadiaServer {
-
-    public static final int SERVER_WIDTH = 856, SERVER_HEIGHT = 480;
+public class BlockadiaServer extends MainExtras {
 
     public static void main(String[] args) {
-        Display display = new Display("Blockadia Server", SERVER_WIDTH, SERVER_HEIGHT);
+        Display display = new Display("Blockadia Server", getWidth(), getHeight());
         display.run();
     }
 
