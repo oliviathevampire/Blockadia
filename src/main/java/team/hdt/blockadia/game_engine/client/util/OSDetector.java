621bace12eccf92afd2b5228c6e3ab567abd23ee
Mystic4pheonix <Lukethemodded@gmail.com>
2018-09-20 11:15:50 +0100

    static non static modes added

--- src/main/java/team/hdt/blockadia/game_engine/client/util/OSDetector.java
+++ src/main/java/team/hdt/blockadia/game_engine/client/util/OSDetector.java
@@ -3,7 +3,7 @@ package team.hdt.blockadia.game_engine.client.util;
 public class OSDetector {
     private static String OS;
 
-    public int IsOS() {
+    public int nonStaticIsOS() {
         OS = System.getProperty("os.name").toLowerCase();
         if (isWindows()) {
             return 1;
@@ -17,8 +17,24 @@ public class OSDetector {
             return 0;
         }
     }
+  	public static int staticIsOS()
+  	{
+  	  OS = System.getProperty("os.name").toLowerCase();
+        if (isWindows()) {
+            return 1;
+        } else if (isMac()) {
+            return 2;
+        } else if (isUnix()) {
+            return 3;
+        } else if (isSolaris()) {
+            return 4;
+        } else {
+            return 0;
+        }
+  	}
+  
 
-    private boolean isWindows() {
+    private static boolean isWindows() {
 
         return (OS.indexOf("win") >= 0);
 
