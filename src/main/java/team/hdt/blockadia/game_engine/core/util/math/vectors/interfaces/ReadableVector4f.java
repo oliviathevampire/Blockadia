2cad1df431f2ee5eaee962428a0978e6c709b6eb
Mystic4pheonix <lukethemodded@gmail.com>
2018-09-26 19:25:10 +0100

    update

--- src/main/java/team/hdt/blockadia/game_engine/core/util/math/vectors/interfaces/ReadableVector4f.java
+++
@@ -1,5 +0,0 @@
-package team.hdt.blockadia.game_engine.core.util.math.vectors.interfaces;
-
-public abstract interface ReadableVector4f extends ReadableVector3f {
-    public abstract float getW();
-}
